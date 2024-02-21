f9d69c2f622b81fb0ab3799cad890e1a8587a9c8
Glanhelmion <kira_syaoran@live.com>
2024-02-21 17:22:24 +0800

    updated server.js and included vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -2,43 +2,29 @@ import express from "express";
 import cookieParser from 'cookie-parser';
 import connectDB from "./dbconfig.js";
 import cors from "cors";
-import authRoutes from "./routes/auth.js";// Import the auth routes
+import authRoutes from "./routes/auth.js";
 import { accessoryRouter, birdRouter, catRouter, dogRouter, fishRouter, petfoodRouter } from "./routes/productRouter.js";
 import dotenv from "dotenv";
 
 const app = express();
 
-
-dotenv.config(); //to connect and pull data from .env
+dotenv.config(); // Load environment variables from .env file
 connectDB(); // Establish database connection to MongoDB
 
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-//cookie parser middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// Enable CORS
 app.use(cors());
 
-
-// Use the authentication routes
 app.use("/api/auth", authRoutes);
-
-// Routes
-app.use("/api/accessoriespage", accessoryRouter); // Mount the accessoryRouter at /api/accessories
-app.use("/api/birdpage", birdRouter); // Mount the birdRouter at /api/birds
-app.use("/api/catpage", catRouter); // Mount the catRouter at /api/cats
-app.use("/api/dogpage", dogRouter); // Mount the dogRouter at /api/dogs
-app.use("/api/fishpage", fishRouter); // Mount the fishRouter at /api/fishes
+app.use("/api/accessoriespage", accessoryRouter);
+app.use("/api/birdpage", birdRouter);
+app.use("/api/catpage", catRouter);
+app.use("/api/dogpage", dogRouter);
+app.use("/api/fishpage", fishRouter);
 app.use("/api/petfoodpage", petfoodRouter);
 
-// Start the server
-app.listen(5000, () => {
-  console.log("Port 5000 connected");
+const PORT = process.env.PORT || 5000; // Use the dynamic port provided by Vercel or default to 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-// Below is Ritchie"s code for "Start the server"
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
